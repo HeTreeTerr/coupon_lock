a8c7b9cc87eda8c6e9e2dd7398a7c45edb0c1534
‘HeTreeTerr’ <3110708879@qq.com>
2021-11-21 13:45:55 +0800

    售货v1.0.0

--- src/main/java/com/hss/controller/handWrittenDistributedLock/DistributedLockDemoController.java
+++ src/main/java/com/hss/controller/handWrittenDistributedLock/DistributedLockDemoController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * 手动实现分布式锁
  */
@@ -25,9 +27,32 @@ public class DistributedLockDemoController {
     @Autowired
     private RedisTemplate redisTemplate;
 
+    private final static String GOODKEY = "good:001";
+
+    /**
+     * 1.低并发先，没有问题
+     * 2.高并发下，存在超卖超买问题
+     *  原因：可见性、原子性、指令重排
+     * @return
+     */
     @RequestMapping(value = "/v1.0.0")
     public String buyGoodsV1_0_0(){
-        logger.info("{}==buyGoods==V1_0_0",serverPort);
-        return "success";
+//        1.查询库存
+        Integer number = Integer.valueOf(redisTemplate.opsForValue().get(GOODKEY).toString());
+//        2.库存充足
+        if(number > 0)
+        {
+//            3.执行售货逻辑
+            logger.info("服务{}====={}号商品，出售成功!",serverPort,number);
+            try {
+                TimeUnit.MILLISECONDS.sleep(20);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+//            4.库存减一
+            redisTemplate.opsForValue().set(GOODKEY,number-1);
+            return number + "号商品，出售成功!";
+        }
+        return "商品已售完，库存不足";
     }
 }
