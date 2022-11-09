eb2e434531d63b79465f42c9bb3f2656d8f5341d
hss1552 <hesensen@tzt.cn>
2022-11-09 15:16:52 +0800

    手动实现看门狗机制

---
+++ src/main/java/com/hss/util/WatchDogManager.java
@@ -0,0 +1,66 @@
+package com.hss.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Enumeration;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 实现看门狗机制
+ * 线程监控
+ */
+public class WatchDogManager {
+
+    private static final Logger logger = LoggerFactory.getLogger(WatchDogManager.class);
+
+    /**
+     * 存放受监控的线程
+     */
+    private static ConcurrentHashMap<Long, Thread> map = new ConcurrentHashMap();
+
+    /**
+     * 监控线程
+     * @param thread
+     */
+    public static void watch(Thread thread){
+        map.put(thread.getId(),thread);
+    }
+
+    /**
+     * 线程结束，解除监控
+     * @param thread
+     */
+    public static void unWatch(Thread thread){
+        map.remove(thread.getId());
+    }
+
+    /**
+     * 启动一个线程，专门做线程监视
+     */
+    public static void start(){
+        new Thread(()->{
+                while (true){
+                    Enumeration<Long> keys = map.keys();
+                    while (keys.hasMoreElements()){
+                        Long aLong = keys.nextElement();
+                        Thread thread = map.get(aLong);
+                        logger.info("thread:{}",thread);
+                        if(thread != null){
+                            logger.info("thread:{}监听",aLong);
+                        }else{
+                            map.remove(aLong);
+                            logger.info("thread:{}不监听",aLong);
+                        }
+                    }
+                    try {
+                        TimeUnit.SECONDS.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+        }).start();
+    }
+
+}
---
+++ src/test/java/com/hss/util/WatchDogManagerTest.java
@@ -0,0 +1,50 @@
+package com.hss.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 看门狗基本使用
+ */
+public class WatchDogManagerTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(WatchDogManagerTest.class);
+
+    public static void main(String[] args) throws InterruptedException {
+        //启动看门狗线程
+        WatchDogManager.start();
+
+        //启动五个执行耗时随机的线程
+        for (int i = 0; i < 5; i++) {
+            Runnable runnable = ()->{
+                try {
+                    TimeUnit.SECONDS.sleep(5 + genRandom());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }finally {
+                    //线程结束，不监听
+                    logger.info("{} is end",Thread.currentThread().getName());
+                    WatchDogManager.unWatch(Thread.currentThread());
+                }
+            };
+            Thread thread = new Thread(runnable, "My-Thread-" + i);
+            //监听
+            WatchDogManager.watch(thread);
+            thread.start();
+        }
+
+        //主线程休眠两分钟，观察看门狗线程日志
+        TimeUnit.MINUTES.sleep(2);
+    }
+
+    /**
+     * 产生随机整数 0 <= a < VOLATILITY_EXPIRE_TIME
+     * @return
+     */
+    private static Integer genRandom(){
+        int randomNum = (int)(Math.random() * 20);
+        return randomNum;
+    }
+}
